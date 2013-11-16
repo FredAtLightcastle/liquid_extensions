f2e2e16de333a1285ab85e2eb7a1cd1a81338176
Fred McDavid <fred@landmetrics.com>
2013-11-15 23:14:57 -0500

    adding photo categories tag

---
+++ lib/locomotive/liquid_extensions/filters/photo_categories.rb
@@ -0,0 +1,14 @@
+module Locomotive
+  module LiquidExtensions
+    module Filters
+      module PhotoCategories
+
+        def PhotoCategories(input)
+          Rails.logger.info "input: #{input}"
+        end
+
+      end
+
+    end
+  end
+end
